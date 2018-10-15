af7996942f7269dd2e85d53cbcb57a6b435bf658
vfarah-if <sefrica@gmail.com>
2018-10-15 08:08:28 +0100

    Added Sorting into the Domain

---
+++ src/Git.Domain/SortIt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Git.Domain
+{
+    public static class SortIt<TSource>
+    {
+        public static SortIt<TSource, TKey> With<TKey>
+            (Func<TSource, TKey> projection)
+        {
+            return new SortIt<TSource, TKey>(projection);
+        }
+    }
+
+    public class SortIt<TSource, TKey> : IComparer<TSource>
+    {
+        readonly Func<TSource, TKey> projection;
+        readonly IComparer<TKey> comparer;
+
+        public SortIt(Func<TSource, TKey> projection)
+            : this(projection, null)
+        {
+        }
+
+        public SortIt(Func<TSource, TKey> projection,
+            IComparer<TKey> comparer)
+        {
+            this.comparer = comparer ?? Comparer<TKey>.Default;
+            this.projection = projection ?? throw new ArgumentNullException(nameof(projection));
+        }
+
+        public int Compare(TSource x, TSource y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            if (x == null)
+            {
+                return -1;
+            }
+            return y == null ? 1 : comparer.Compare(projection(x), projection(y));
+        }
+    }
+}
---
+++ tests/Git.Domain.UnitTests/SortItShould.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace Git.Domain.UnitTests
+{
+    public class SortItShould
+    {
+        private Fixture autoFixture;
+
+        public SortItShould()
+        {
+            autoFixture = new Fixture();
+        }
+
+        [Fact]
+        public void SortStringsByDefaultComparer()
+        {
+            var items = CreateTestAllSortsList();
+
+            SortIt<TestAllSorts, string> stringSort = SortIt<TestAllSorts>.With(sortField => sortField.StringField);
+
+            items.Sort(stringSort);
+
+            items[0].StringField.Should().Be("aaaa");
+            items[1].StringField.Should().Be("bbbb");
+            items[2].StringField.Should().Be("cccc");
+        }
+
+        [Fact]
+        public void SortIntegersByDefaultComparer()
+        {
+            var items = CreateTestAllSortsList();
+
+            SortIt<TestAllSorts, int> intSort = SortIt<TestAllSorts>.With(sortField => sortField.IntegerField);
+
+            items.Sort(intSort);
+
+            items[0].IntegerField.Should().Be(1);
+            items[1].IntegerField.Should().Be(2);
+            items[2].IntegerField.Should().Be(3);
+        }
+
+        private List<TestAllSorts> CreateTestAllSortsList()
+        {
+            var items = new List<TestAllSorts>
+            {
+                new TestAllSorts()
+                {
+                    StringField = "cccc",
+                    IntegerField = 3
+                },
+                new TestAllSorts()
+                {
+                    StringField = "bbbb",
+                    IntegerField = 2
+                },
+                new TestAllSorts()
+                {
+                    StringField = "aaaa",
+                    IntegerField = 1
+                }
+            };
+            return items;
+        }
+    }
+
+    internal class TestAllSorts
+    {
+        public string StringField { get; set; }
+        public int IntegerField { get; set; }
+    }
+}
