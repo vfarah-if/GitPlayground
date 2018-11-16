94931d63a74e34b4d2f8b29ed5379d3775956ab5
vfarah-if <sefrica@gmail.com>
2018-11-16 17:34:36 +0000

    Initial play with interfaces become more configurable

--- src/ngAccidentStatistics/src/app/accident-statistic/accident-statistic-map/accident-statistic-map.component.ts
+++ src/ngAccidentStatistics/src/app/accident-statistic/accident-statistic-map/accident-statistic-map.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 
 import { Observable } from 'rxjs/internal/Observable';
-import { EMPTY } from 'rxjs/internal/observable/empty';
 import { expand, map, reduce } from 'rxjs/internal/operators';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { EMPTY } from 'rxjs/internal/observable/EMPTY';
 
 import { tileLayer, latLng, circle, polygon, marker, Map, MapOptions, Control, icon } from 'leaflet';
 
@@ -19,8 +19,9 @@ import 'leaflet/dist/images/marker-icon.png';
   styleUrls: ['./accident-statistic-map.component.scss']
 })
 export class AccidentStatisticMapComponent implements OnInit, OnDestroy {
-  @Input() fromDate: Date;
-
+  @Input() fromDate: string;
+  @Input() imageType: 'heatmap' | 'macarbe' | 'friendly';
+  from: Date;
   leafletOptions: MapOptions = {
     layers: [
       tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 })
@@ -30,10 +31,10 @@ export class AccidentStatisticMapComponent implements OnInit, OnDestroy {
   };
 
   layersControl = {
-    baseLayers: {
-      'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 })
-      //  , 'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18 })
-    }
+    // baseLayers: {
+    //   'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18 })
+    //    , 'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18 })
+    // }
     // , overlays: {
     //   'Big Circle': circle([46.95, -122], { radius: 5000 }),
     //   'Big Square': polygon([[46.8, -121.55], [46.9, -121.55], [46.9, -121.7], [46.8, -121.7]]),
@@ -41,26 +42,59 @@ export class AccidentStatisticMapComponent implements OnInit, OnDestroy {
     // }
   };
 
-  accidentStatics$: Observable<Array<AccidentStatistic>>;
-  accidentStatisticsFirstPage$: Observable<PagedAccidentStatistic>;
+  public accidentStatisticsFirstPage$: Observable<PagedAccidentStatistic>;
+  private accidentStatics$: Observable<Array<AccidentStatistic>>;
   private subscription: Subscription;
+  private mapIcon: icon;
 
   constructor(private accidentStatisticService: AccidentStatiticsService) { }
 
   ngOnInit() {
-    if (!this.fromDate) {
-      this.fromDate = new Date(2010, 1, 1);
+    if (this.fromDate) {
+      this.from = new Date(this.fromDate);
+    } else {
+      this.from = new Date(2010, 1, 1);
     }
-    this.accidentStatisticsFirstPage$ = this.accidentStatisticService.get({ pageSize: 500, from: this.fromDate });
 
-    this.accidentStatics$ = this.accidentStatisticsFirstPage$
-      .pipe(
+    if (!this.imageType) {
+      this.imageType = 'macarbe';
+    }
+
+    this.setMapIcon();
+
+    this.accidentStatisticsFirstPage$ = this.accidentStatisticService.get({ pageSize: 500, from: this.from });
+
+    this.accidentStatics$ = this.accidentStatisticsFirstPage$.pipe(
         expand(({ nextPage }) => {
-          return nextPage ? this.accidentStatisticService.get({ pageSize: 500, from: this.fromDate, page: nextPage }) : EMPTY;
+          return nextPage
+            ? this.accidentStatisticService.get({ pageSize: 500, from: this.from, page: nextPage })
+            : EMPTY;
         }),
         map(({ data }) => data),
         reduce((acc, data) => acc.concat(data), [])
       );
+  }
+
+  setMapIcon() {
+    switch (this.imageType) {
+      case 'heatmap':
+        this.mapIcon = null;
+        break;
+      case 'friendly':
+        this.mapIcon = icon({
+          iconSize: [25, 25],
+          iconAnchor: [13, 25],
+          iconUrl: 'https://image.flaticon.com/icons/svg/130/130163.svg'
+        });
+        break;
+      default:
+        this.mapIcon = icon({
+          iconSize: [25, 25],
+          iconAnchor: [13, 25],
+          iconUrl: 'https://static.thenounproject.com/png/14312-200.png'
+        });
+        break;
+    }
   }
 
   ngOnDestroy(): void {
@@ -70,19 +104,14 @@ export class AccidentStatisticMapComponent implements OnInit, OnDestroy {
   }
 
   onMapReady(leafMap: Map) {
-    console.log('Start mapping away with the service ...');
     this.subscription = this.accidentStatics$.subscribe(data => {
       data.forEach(item => {
-        marker([Number(item.lat), Number(item.lon)]
-          // Comment this out to see the default map
-          , {
-            icon: icon({
-              iconSize: [25, 41],
-              iconAnchor: [13, 41],
-              iconUrl: 'https://static.thenounproject.com/png/14312-200.png',
-            })
+          if (this.mapIcon) {
+            marker([Number(item.lat), Number(item.lon)], { icon: this.mapIcon }).addTo(leafMap);
+          } else {
+            marker([Number(item.lat), Number(item.lon)]).addTo(leafMap);
           }
-        ).addTo(leafMap);
+        }
       });
     });
   }
