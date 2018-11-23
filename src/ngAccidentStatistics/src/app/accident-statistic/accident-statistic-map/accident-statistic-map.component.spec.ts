8caa3fb5d4f4cf106d3b26aabe78fb731a6e4c7f
vfarah-if <sefrica@gmail.com>
2018-11-23 14:08:26 +0000

    Prepare map for Unit testing

--- src/ngAccidentStatistics/src/app/accident-statistic/accident-statistic-map/accident-statistic-map.component.spec.ts
+++ src/ngAccidentStatistics/src/app/accident-statistic/accident-statistic-map/accident-statistic-map.component.spec.ts
@@ -1,25 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 import { AccidentStatisticMapComponent } from './accident-statistic-map.component';
+import {
+  ACCIDENT_STATISTIC_SERVICE_PROVIDER,
+  AccidentStatiticsServiceMock,
+  getAccidentStatiticsService
+} from './../../api/testing';
 
 describe('AccidentStatisticMapComponent', () => {
   let component: AccidentStatisticMapComponent;
   let fixture: ComponentFixture<AccidentStatisticMapComponent>;
+  let apiService: AccidentStatiticsServiceMock;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AccidentStatisticMapComponent ]
-    })
-    .compileComponents();
+      imports: [LeafletModule],
+      declarations: [AccidentStatisticMapComponent],
+      providers: [
+        ACCIDENT_STATISTIC_SERVICE_PROVIDER()
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AccidentStatisticMapComponent);
     component = fixture.componentInstance;
+    apiService = getAccidentStatiticsService();
+    apiService.spy_get.and.returnValues(apiService.simplePageOneOfTwoResponseSubject, apiService.simplePageTwoOfTwoResponseSubject);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(apiService).toBeTruthy();
   });
 });
