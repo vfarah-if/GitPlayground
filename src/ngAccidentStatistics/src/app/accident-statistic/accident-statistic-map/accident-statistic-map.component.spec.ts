acd1957f59ab5dca55fd13dc2c7e09eaf82a00ac
vfarah-if <sefrica@gmail.com>
2018-11-26 18:55:39 +0000

    Fixed happy path basic map tests before doing pluralization changes

--- src/ngAccidentStatistics/src/app/accident-statistic/accident-statistic-map/accident-statistic-map.component.spec.ts
+++ src/ngAccidentStatistics/src/app/accident-statistic/accident-statistic-map/accident-statistic-map.component.spec.ts
@@ -10,11 +10,27 @@ import {
 } from './../../api/testing';
 import { DEFAULT_FROM_DATE } from '..';
 
-describe('AccidentStatisticMapComponent', () => {
+function headerElement(compiled): HTMLHeadingElement {
+  return compiled.querySelector('section > header > h1');
+}
+
+function imageMarkerElements(compiled): Array<HTMLImageElement> {
+  return compiled.querySelectorAll('section > div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-marker-pane > img');
+}
+
+function popupDivElement(compiled): HTMLDivElement {
+  // tslint:disable-next-line:max-line-length
+  return compiled.querySelector('section > div > div.leaflet-pane.leaflet-map-pane > div.leaflet-pane.leaflet-popup-pane > div > div.leaflet-popup-content-wrapper > div');
+}
+
+
+
+fdescribe('AccidentStatisticMapComponent', () => {
   let component: AccidentStatisticMapComponent;
   let fixture: ComponentFixture<AccidentStatisticMapComponent>;
   let apiService: AccidentStatiticsServiceMock;
   let lastYear: number;
+  let compiled: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -26,7 +42,7 @@ describe('AccidentStatisticMapComponent', () => {
     }).compileComponents();
   }));
 
-  fdescribe('Given default settings', () => {
+  describe('Given default settings', () => {
     beforeEach(() => {
       fixture = TestBed.createComponent(AccidentStatisticMapComponent);
       component = fixture.componentInstance;
@@ -34,9 +50,10 @@ describe('AccidentStatisticMapComponent', () => {
       apiService.spy_get.and.returnValues(apiService.simplePageOneOfTwoResponseSubject, apiService.simplePageTwoOfTwoResponseSubject);
       lastYear = new Date().getFullYear() - 1;
       fixture.detectChanges();
+      compiled = fixture.debugElement.nativeElement;
     });
 
-    fit('should create with default expectations', () => {
+    it('should create with default expectations', () => {
       expect(component).toBeTruthy();
       expect(component.from).toBe(DEFAULT_FROM_DATE);
       expect(component.to.toISOString()).toBe(`${lastYear}-12-31T12:00:00.000Z`);
@@ -45,12 +62,50 @@ describe('AccidentStatisticMapComponent', () => {
       expect(component.imageOption).toBe('Macarbe');
       expect(component.zoom).toBe(9);
       expect(component.maxZoom).toBe(18);
+      expect(component.useGeolocationPosition).toBe(false);
       expect(apiService).toBeTruthy();
     });
 
-    // TODO: Test the header
-    // TODO: Test the two positions on the map
-    // TODO: Test the popup on the map when click displays expected message
+    it('should create default header', async(() => {
+      const header = headerElement(compiled);
+      expect(header).toBeTruthy();
+      // tslint:disable-next-line:max-line-length
+      const expectedHeader = `Map of fatal accidents from Feb 1, 2010, 12:00:00 AM to Dec 31, ${lastYear}, 12:00:00 PM (2 Total)`;
+      expect(header.innerText).toBe(expectedHeader);
+    }));
+
+    it('should create exactly two fatal image markers on the map', async(() => {
+      const images = imageMarkerElements(compiled);
+      const expectedMacarbeImageUrl = 'https://static.thenounproject.com/png/14312-200.png';
+
+      expect(images).toBeTruthy();
+      expect(images.length).toBe(2);
+      expect(images[0].src).toBe(expectedMacarbeImageUrl);
+      expect(images[1].src).toBe(expectedMacarbeImageUrl);
+    }));
+
+    it('should create a pop up on the first image describing the incident when clicked', async(() => {
+      const images = imageMarkerElements(compiled);
+
+      images[0].click();
+
+      const popupElement = popupDivElement(compiled);
+      expect(popupElement).toBeTruthy();
+      // tslint:disable-next-line:max-line-length
+      expect(popupElement.innerText).toContain('Fatal Incident 632474, occured on Thu Jan 05 2017 09:11:00 GMT+0000 (Greenwich Mean Time), involving 1 person(s) and 2 vehicle(s) in the borough of Kingston.')
+    }));
+
+    it('should create a pop up on the first image describing the incident when clicked', async(() => {
+      const images = imageMarkerElements(compiled);
+
+      images[1].click();
+
+      const popupElement = popupDivElement(compiled);
+      expect(popupElement).toBeTruthy();
+      // tslint:disable-next-line:max-line-length
+      expect(popupElement.innerText).toContain('Fatal Incident 615289, occured on Fri Dec 29 2017 10:58:00 GMT+0000 (Greenwich Mean Time), involving 1 person(s) and 1 vehicle(s) in the borough of City of Westminster.')
+    }));
+
     // TODO: Fix the pop up message to display the accident summary better with pluralizations
     // TODO: Remove the fdescribe and for other tests to run
   });
