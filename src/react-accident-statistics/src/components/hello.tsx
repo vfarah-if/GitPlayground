3b7193ca188812bc6e4e4ec62e002e0a6005e691
vfarah-if <sefrica@live.com>
2018-11-28 17:36:42 +0000

    Extended the hello to add a function

--- src/react-accident-statistics/src/components/hello.tsx
+++ src/react-accident-statistics/src/components/hello.tsx
@@ -6,22 +6,43 @@ export interface Props {
     enthusiasmLevel?: number;
 }
 
-class Hello extends React.Component<Props, object> {
+// tslint:disable-next-line:interface-name
+interface State {
+    currentEnthusiasm: number;
+}
+
+class Hello extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
+    }
+
+    public onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+
+    public onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+
     public render() {
         const { name, enthusiasmLevel = 1 } = this.props;
 
         if (enthusiasmLevel <= 0) {
             throw new Error('You could be a little more enthusiastic. :D');
         }
-    
+
         return (
             <div className="hello">
                 <div className="greeting">
-                    Hello {name + getExclamationMarks(enthusiasmLevel)}
+                    Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
                 </div>
+                <button onClick={this.onDecrement}>-</button>
+                <button onClick={this.onIncrement}>+</button>
             </div>
         );
     }
+
+    private updateEnthusiasm(currentEnthusiasm: number) {
+        console.log("Entered this function");
+        this.setState({ currentEnthusiasm });
+    };
 }
 
 export default Hello;
