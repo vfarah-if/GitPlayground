294050943379d7fd70ffd61b13af2558f03acf45
vfarah-if <sefrica@live.com>
2018-11-29 10:29:19 +0000

    Created an example of how I will generate a Readonnly repository utilising CQRS patters

--- src/Git.Domain.EntityFramework.ConsoleApp/Program.cs
+++ src/Git.Domain.EntityFramework.ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
+using Git.Domain.Models.TFL;
 using Console = System.Console;
 
 namespace Git.Domain.EntityFramework.ConsoleApp
@@ -17,9 +18,40 @@ namespace Git.Domain.EntityFramework.ConsoleApp
                 Console.WriteLine($"{accidentStatisticDbContext.AccidentStatistics.LongCountAsync().GetAwaiter().GetResult()} Accident Statistic records found");
                 Console.WriteLine($"{accidentStatisticDbContext.Vehicles.LongCountAsync().GetAwaiter().GetResult()} Vehicle records found");
                 Console.WriteLine($"{accidentStatisticDbContext.Casualties.LongCountAsync().GetAwaiter().GetResult()} Casualty records found");
+                LoadAllCycylingAccidents(accidentStatisticDbContext, new DateTime(2005, 01, 01, 1, 1, 1), new DateTime(2017, 12, 31, 1, 1, 1));
                 Console.Read();
             }
         }
+
+        private static void LoadAllCycylingAccidents(AccidentStatisticDbContext accidentStatisticDbContext,
+            DateTime from, DateTime to)
+        {
+            // TODO: Generate a repository that creates the same scenario
+
+            // Count the records
+            var cyclingAccidentCount = accidentStatisticDbContext.AccidentStatistics
+                .Count(accident => 
+                    accident.Date >= from && accident.Date <= to &&
+                    accident.Severity == Severity.Fatal &&
+                    accident.Casualties.Any(casualty => casualty.Mode.Equals("PedalCycle") && casualty.Severity == Severity.Fatal));
+            Console.WriteLine($"{cyclingAccidentCount} cycling fatalities in total from {from.ToLongDateString()} to {to.ToLongDateString()}");
+
+            // Get the details and generate a minor report
+
+            var cyclingAccidents = accidentStatisticDbContext.AccidentStatistics
+                .Where(accident =>
+                    accident.Date >= from && accident.Date <= to &&
+                    accident.Severity == Severity.Fatal &&
+                    accident.Casualties.Any(casualty => casualty.Mode.Equals("PedalCycle") && casualty.Severity == Severity.Fatal))
+                .Include(x => x.Casualties)
+                .Include(x => x.Vehicles)
+                .AsEnumerable();
+            foreach (var cyclingAccident in cyclingAccidents)
+            {
+                Console.WriteLine($"Cycling accident occured in the borough of {cyclingAccident.Borough.ToUpper()} involving '{cyclingAccident.Casualties.Count}' casualties and '{cyclingAccident.Vehicles.Count}' vehicles.");
+            }
+        }
+
 
         private static void LogAllDatabaseActivities(AccidentStatisticDbContext accidentStatisticDbContext)
         {
