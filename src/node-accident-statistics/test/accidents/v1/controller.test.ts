54d838bb492bc3290ef330774a2608f8b785ff2b
vfarah-if <vincent.farah@codurance.com>
2019-02-13 01:44:33 +0000

    Adda done back in to notify finished

--- src/node-accident-statistics/test/accidents/v1/controller.test.ts
+++ src/node-accident-statistics/test/accidents/v1/controller.test.ts
@@ -2,7 +2,7 @@ import request from "supertest";
 import app from "../../../src/server";
 
 describe("GET /v1/accidents", () => {
-    it("should return 200 with default query", async () => {
+    it("should return 200 with default query", async (done) => {
         const response = await request(app).get("/v1/accidents").set("Accept", "application/json");
         // tslint:disable-next-line:no-console
         // console.log("/v1/accidents Responded with => ", response);
@@ -13,9 +13,10 @@ describe("GET /v1/accidents", () => {
         expect(response.body.from).toBe("2017-01-01T00:00:00.000Z");
         expect(response.body.to).toBe("2017-12-31T00:00:00.000Z");
         expect(response.body.severity).toBe("Fatal");
+        done();
     });
 
-    it("should return 200 with query parameters", async () => {
+    it("should return 200 with query parameters", async (done) => {
         const response = await request(app)
         .get("/v1/accidents?page=2&pageSize=90&orderBy=BoroughDescending&severity=Slight")
         .set("Accept", "application/json");
@@ -28,5 +29,6 @@ describe("GET /v1/accidents", () => {
         expect(response.body.from).toBe("2017-01-01T00:00:00.000Z");
         expect(response.body.to).toBe("2017-12-31T00:00:00.000Z");
         expect(response.body.severity).toBe("Slight");
+        done();
     });
 });
