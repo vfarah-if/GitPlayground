250ab02d0ab76a7fd7d34bbdab30f7c9df7dca5c
vfarah-if <vincent.farah@codurance.com>
2019-02-14 06:08:12 +0000

    Unable to  reproduce the validation issues in tests

--- src/node-accident-statistics/src/accidents/v1/client/transportForLondonClient.ts
+++ src/node-accident-statistics/src/accidents/v1/client/transportForLondonClient.ts
@@ -21,7 +21,7 @@ export class TransportForLondonClient {
         );
     }
 
-    public async getAccidentStatistics(
+    private async getAccidentStatistics(
         from: Date,
         to: Date,
         severity: string,
@@ -31,10 +31,6 @@ export class TransportForLondonClient {
 
         const maxYear = Number(process.env.MAX_YEAR);
 
-        if (!from) {
-            this.raiseRequired("from");
-        }
-
         if (from && from.getFullYear() < 2005) {
             this.raiseDataBelow2005();
         }
@@ -43,33 +39,10 @@ export class TransportForLondonClient {
             this.raiseMaxYearNotSupported(maxYear);
         }
 
-        if (!to) {
-            this.raiseRequired("to");
-        }
-
-        if (to && to.getFullYear() < 2005) {
-            this.raiseDataBelow2005();
-        }
-
         if (to && to.getFullYear() > maxYear) {
             this.raiseMaxYearNotSupported(maxYear);
         }
 
-        if (!severity) {
-            this.raiseRequired("severity");
-        }
-
-        if (!sortBy) {
-            this.raiseRequired("sortBy");
-        }
-
-        if (!page) {
-            this.raiseRequired("page");
-        }
-
-        if (!pageSize) {
-            this.raiseRequired("pageSize");
-        }
         // tslint:disable-next-line:no-console
         console.log("Retrieving year data from cache or TFL live site");
         // TODO: Hook up Axios to call the TFL api by each year and expose a generic predicate mechanism
