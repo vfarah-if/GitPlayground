a2e6c4082f5c1ca09f4957cc49f029ab5c5d64a2
vfarah-if <vincent.farah@codurance.com>
2019-02-15 17:49:37 +0000

    Fixed the controller tests to use knock

--- src/node-accident-statistics/test/accidents/v1/controller.test.ts
+++ src/node-accident-statistics/test/accidents/v1/controller.test.ts
@@ -1,7 +1,21 @@
+
+import nock from "nock";
 import request from "supertest";
 import app from "../../../src/server";
+import { default as testData } from "./controller.test.data.json";
 
 describe("GET /v1/accidents", () => {
+    beforeAll(() => {
+        process.env.TFL_ACCIDENTS_URL = "https://api.tfl.gov.uk/AccidentStats";
+        nock("https://api.tfl.gov.uk")
+            .get("/AccidentStats/2017")
+            .reply(200, testData);
+    });
+
+    it("should create test with all expectations", () => {
+        expect(testData).toBeTruthy();
+    });
+
     it("should return 200 with default query", async (done) => {
         const response = await request(app).get("/v1/accidents");
         // tslint:disable-next-line:no-console
