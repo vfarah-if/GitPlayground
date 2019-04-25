7991a58869085d661e52608d0791d191fa2b8081
vfarah-if <sefrica@live.com>
2019-04-25 13:57:26 +0100

    Extended the tests for the Hello World app

--- src/ng-demo/src/app/hello-world/hello-world.component.spec.ts
+++ src/ng-demo/src/app/hello-world/hello-world.component.spec.ts
@@ -2,24 +2,37 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloWorldComponent } from './hello-world.component';
 
+function paragraphElement(compiled): HTMLParagraphElement {
+  return compiled.querySelector('section > p');
+}
+
 describe('HelloWorldComponent', () => {
   let component: HelloWorldComponent;
   let fixture: ComponentFixture<HelloWorldComponent>;
+  let compiled;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HelloWorldComponent ]
+      declarations: [HelloWorldComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HelloWorldComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(compiled).toBeTruthy();
+  });
+
+  it('should output hello world', () => {
+    const element = paragraphElement(compiled);
+    expect(element).toBeTruthy();
+    expect(element.innerText).toContain('Hello World');
   });
 });
