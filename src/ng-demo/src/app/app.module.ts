f6664fda5affb5a227faf334ccb4c624c574e035
vfarah-if <sefrica@live.com>
2019-04-25 14:47:53 +0100

    Updated styles.scss

--- src/ng-demo/src/app/app.module.ts
+++ src/ng-demo/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldModule } from './hello-world/hello-world.module';
+import { HelloWorldModule } from './hello-world';
 
 @NgModule({
   declarations: [
