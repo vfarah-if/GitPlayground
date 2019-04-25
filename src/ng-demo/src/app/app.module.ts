42693a084022f0e9948b998c4e213827bcb0b37e
vfarah-if <sefrica@live.com>
2019-04-25 12:35:55 +0100

    Added simple styles to be output

--- src/ng-demo/src/app/app.module.ts
+++ src/ng-demo/src/app/app.module.ts
@@ -4,17 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldModule } from './hello-world/hello-world.module';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // HelloWorldModule
+    HelloWorldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
